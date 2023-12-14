0ff86861d4da30bf2c3941f5c932def90422f833
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-12-14 20:17:58 +0530

    Delete Functionality after enrollment error

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -16,6 +17,7 @@ func operations(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Hello I am Student Management API</h1>")
 	case "/delete":
 		fmt.Fprint(w, "<h1>Delete Student</h1>")
+		deleteStudent(w, r)
 	case "/add":
 		fmt.Fprint(w, "<h1>Add Student</h1>")
 		addStudent(w, r)
@@ -25,10 +27,11 @@ func operations(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Error")
 	}
 }
+
 func addStudent(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
 	age := r.FormValue("age")
-	enrollment := r.FormValue("enrolllment")
+	enrollment := r.FormValue("enrollment")
 	var err error
 	_, err = db.Exec("INSERT INTO students (name, age, enrollment) VALUES (?, ?, ?)", name, age, enrollment)
 	if err != nil {
@@ -40,6 +43,20 @@ func addStudent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func deleteStudent(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		fmt.Println("Invalid ID type")
+	}
+	_, err = db.Exec("DELETE FROM students WHERE id=?", id)
+	if err != nil {
+		fmt.Println("Error seleteing student details")
+	}
+	if err == nil {
+		fmt.Println("Student details deleted successfully")
+	}
+}
 func createTable() {
 	newTable := `CREATE TABLE students(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), age INTEGER, enrollment VARCHAR(255));`
 	_, err := db.Exec(newTable)
