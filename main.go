e11c23e76588450fc0336b285bd8e28d1d3d5583
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-12-14 18:31:20 +0530

    Intialize the project. Set up routes

---
+++ main.go
@@ -0,0 +1,21 @@
+package nain
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func operations(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "<h1>Hello I am default</h1>")
+	case "/delete":
+		fmt.Fprint(w, "<h1>Delete Student</h1>")
+	case "/add":
+		fmt.Fprint(w, "<h1>Add Student</h1>")
+	case "/update":
+		fmt.Fprint(w, "<h1>Update</h1>")
+	default:
+		fmt.Fprint(w, "Error")
+	}
+}
