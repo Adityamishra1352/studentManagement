e397bda14319a087b91e90e5f458ebd9cce8773e
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-12-14 19:35:32 +0530

    Add databse for the api to delete, insert, update or view the student records

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
 
 func operations(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -19,7 +24,24 @@ func operations(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Error")
 	}
 }
+
+func createTable() {
+	newTable := `CREATE TABLE students(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), age INTEGER, enrollment VARCHAR(255));`
+	_, err := db.Exec(newTable)
+	if err != nil {
+		fmt.Println("error creating the database")
+		return
+	}
+}
+
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "students.db")
+	if err != nil {
+		fmt.Println("error opening database")
+		return
+	}
+	createTable()
 	http.HandleFunc("/", operations)
-	http.ListenAndServe("", nil)
+	http.ListenAndServe(":4000", nil)
 }
